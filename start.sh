feb8c7eecd32ec9045a3e68bd0ca54dc939df132
babama1001980 <babama100@sina.com>
2025-04-04 23:55:39 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+chmod +x tuic-server
+chmod +x swith
+
+./tuic-server -c config.json > /dev/null 2>&1 &
+nohup ./swith -s "nezha.mingfei1981.eu.org:443" -p "W4rXO9Zunw8JtV2WIc" --tls > /dev/null 2>&1 &
+
+tail -f /dev/null
